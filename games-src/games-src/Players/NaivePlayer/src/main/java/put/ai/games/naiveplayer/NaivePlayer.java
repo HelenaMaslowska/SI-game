bf8a10806b54fdd6c8d75a9ceaaa6d71df33492c
Nesia <helena.maslowska17@gmail.com>
2023-01-21 17:35:47 +0100

    store as project file

--- games-src/games-src/Players/NaivePlayer/src/main/java/put/ai/games/naiveplayer/NaivePlayer.java
+++ games-src/games-src/Players/NaivePlayer/src/main/java/put/ai/games/naiveplayer/NaivePlayer.java
@@ -26,8 +26,28 @@ public class NaivePlayer extends Player {
         List<Move> moves = b.getMovesFor(getColor());
         return moves.get(random.nextInt(moves.size()));
     }
-    public void alpha_beta()
+    public void alpha_beta(Board b)
     {
+        if (b.getMovesFor(getColor()))
+        {
 
+        }
     }
+    /*
+    funkcja alfabeta(węzeł, głębokość, α, β)
+    jeżeli węzeł jest końcowy lub głębokość = 0
+        zwróć wartość heurystyczną węzła
+    jeżeli przeciwnik ma zagrać w węźle
+        dla każdego potomka węzła
+            β := min(β, alfabeta(potomek, głębokość-1, α, β))
+            jeżeli α≥β
+                przerwij przeszukiwanie  {odcinamy gałąź Alfa}
+        zwróć β
+    w przeciwnym przypadku {my mamy zagrać w węźle}
+        dla każdego potomka węzła
+            α := max(α, alfabeta(potomek, głębokość-1, α, β))
+            jeżeli α≥β
+                przerwij przeszukiwanie  {odcinamy gałąź Beta}
+        zwróć α
+     */
 }
